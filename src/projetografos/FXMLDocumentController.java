b0d99aecd35be3c8e50ef9a1424306792f831eaa
guilh <guilh@DESKTOP-U3A4A70>
2020-08-23 15:11:41 -0300

    primeiro

---
+++ src/projetografos/FXMLDocumentController.java
@@ -0,0 +1,70 @@
+package projetografos;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+
+
+public class FXMLDocumentController implements Initializable 
+{
+    @FXML
+    private AnchorPane pnPrincipal;
+    List<Circle> Lista;
+    
+    private void handleButtonAction(ActionEvent event) 
+    {
+        
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) 
+    {
+        Lista = new ArrayList();
+    }    
+
+    @FXML
+    private void evtLigaCirculo(MouseEvent event) 
+    {
+        
+    }
+
+    @FXML
+    private void evtCriaLinha(MouseEvent event) 
+    {
+        
+    }
+
+    @FXML
+    private void evtCriaCirculo(MouseEvent event) 
+    {
+        if(Lista.size()<10)
+        {
+            Circle c = new Circle();
+            Label b = new Label();
+
+            c.setCenterX(event.getSceneX());
+            c.setCenterY(event.getSceneY());
+            c.setRadius(20);
+
+            c.setFill(Paint.valueOf("#FFFFFF"));
+            c.setStroke(Paint.valueOf("#000000"));
+            Lista.add(c);
+            b.setText("v"+Lista.size());
+            b.setTranslateX(c.getCenterX()-6);
+            b.setTranslateY(c.getCenterY()-6);
+            pnPrincipal.getChildren().addAll(c,b); 
+        }
+        
+    }
+    
+}
