043516d162abecb636f0400d3ebba0e285260d0a
guilh <guilh@DESKTOP-U3A4A70>
2020-08-24 21:47:55 -0300

    Delete feito e entre outros

---
+++ src/projetografos/ClassesAuxiliares/GeraLista.java
@@ -0,0 +1,6 @@
+package projetografos.ClassesAuxiliares;
+
+
+public class GeraLista {
+    
+}
--- src/projetografos/FXMLDocumentController.java
+++ src/projetografos/FXMLDocumentController.java
@@ -1,5 +1,6 @@
 package projetografos;
 
+import com.jfoenix.controls.JFXComboBox;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,7 +8,13 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Cursor;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Paint;
@@ -23,45 +30,73 @@ public class FXMLDocumentController implements Initializable
     private List<Circle> Lista;
     private int ultimo;
     private List<Arestas> LisAre;
-    
-    private void handleButtonAction(ActionEvent event) 
-    {
-        
-    }
-    
+    @FXML
+    private AnchorPane pnextra;
+    @FXML
+    private CheckBox cbValorado;
+    @FXML
+    private CheckBox cbDire;
+    @FXML
+    private JFXComboBox<String> cbLista;
+    @FXML
+    private Label labelUltimo;
+    private int ultimaAresta;
+    private List<Label> ListLabel;
+    @FXML
+    private Label labelUltimoAresta;
+    private boolean flag = false;
+       
     @Override
     public void initialize(URL url, ResourceBundle rb) 
     {
         Lista = new ArrayList();
         LisAre = new ArrayList();
+        ListLabel = new ArrayList();
+        
         this.ultimo=-1;
+        this.ultimaAresta = -1;
+        labelUltimoAresta.setText("Última Aresta Selecionado: "+ultimaAresta);
+        labelUltimo.setText("Último Vertice Selecionado: "+ultimo);
     }   
     
     @FXML
-    private void evtCriaCirculo(MouseEvent event) 
-    {          
-        boolean ok=true;
-        double x,y,aux;      
-        int i;
-        
-        x=event.getSceneX();
-        y=event.getSceneY();
-        for (i = 0; i < Lista.size() && ok; i++)
-        {
-           aux=Math.sqrt(Math.pow((x-Lista.get(i).getCenterX()),2)+Math.pow((y-Lista.get(i).getCenterY()),2));
-           if(Lista.get(i).getRadius()>=aux)
-           {
-               ok=false;
-           }
+    private void evtCriaCirculo(MouseEvent event) {
+        double x, y;
+
+        x = event.getSceneX();
+        y = event.getSceneY();
+
+        if (x < pnextra.getLayoutX() - 15 || y < pnextra.getLayoutY() - 15 && !flag) {
+
+                boolean ok = true;
+                double aux;
+                int i;
+
+                for (i = 0; i < Lista.size() && ok; i++) {
+                    aux = Math.sqrt(Math.pow((x - Lista.get(i).getCenterX()), 2) + Math.pow((y - Lista.get(i).getCenterY()), 2));
+
+                    if (Lista.get(i).getRadius() >= aux) {
+                        ok = false;
+                    }
+                }
+
+                if (ok) {
+                    criaCirculo(event);
+                } else if (ultimo != -1) {
+                    criaLinha(i);
+                } else {
+
+                    ultimo = i;
+                    labelUltimo.setText("Último Vertice Selecionado: " + ultimo);
+                }
+          
+
         }
-        if(ok)
-            criaCirculo(event);
-        else if(ultimo!=-1)
-            criaLinha(i);
         else
-            ultimo=i;
-        
+              flag = false;
     }
+    
+    
     private void criaLinha(int i)
     {
         double xi,yi,xf,yf;
@@ -70,10 +105,30 @@ public class FXMLDocumentController implements Initializable
         xf=Lista.get(i-1).getCenterX();
         yf=Lista.get(i-1).getCenterY();
         Line l= new Line(xi,yi,xf,yf);
+       
+        l.setStrokeWidth(5);
         pnPrincipal.getChildren().add(l);
         LisAre.add(new Arestas(l,ultimo-1,i-1,0));
+        l.setOnMouseEntered((event) -> {
+                l.setCursor(Cursor.HAND);
+                flag = true;
+                
+        });
+        l.setOnMouseClicked((event) -> {
+            
+            for (int j = 0; j < LisAre.size(); j++) {
+                if(LisAre.get(j).getAresta().equals(l))
+                    this.ultimaAresta = j;
+            }
+            labelUltimoAresta.setText("Última Aresta Selecionado: "+ultimaAresta);
+            
+        });
         this.ultimo=-1;
+        labelUltimo.setText("Último Vertice Selecionado: "+ultimo);
+          flag = false;
+
     }
+    
     private void criaCirculo(MouseEvent event)
     {
         double x,y;
@@ -93,17 +148,63 @@ public class FXMLDocumentController implements Initializable
             b.setText("v"+Lista.size());
             b.setTranslateX(c.getCenterX()-6);
             b.setTranslateY(c.getCenterY()-6);
+            ListLabel.add(b);
+            
             pnPrincipal.getChildren().addAll(c,b); 
+            flag = false;
         }
-    }
-    @FXML
-    private void evtCria(MouseEvent event) {
     }
 
     @FXML
-    private void evtCriaLinha(MouseEvent event) 
-    {
+    private void evtLimpaUltimo(ActionEvent event) {
         
+        this.ultimo = -1;
+        labelUltimo.setText("Último Vertice Selecionado: "+ultimo);
+    }
+
+    @FXML
+    private void evtExcluiVertice(KeyEvent event) {
+        
+        if(event.getCode() == KeyCode.DELETE){
+         
+            if(ultimo != -1){
+                
+                Alert a = new Alert(Alert.AlertType.CONFIRMATION, "Deseja excluir esse vertice?",ButtonType.YES, ButtonType.NO);
+                
+                if(a.showAndWait().get() == ButtonType.YES){
+                                        
+                    pnPrincipal.getChildren().remove(Lista.get(ultimo-1));
+                    Lista.remove(ultimo-1);
+                    pnPrincipal.getChildren().remove(ListLabel.get(ultimo-1));
+                    ListLabel.remove(ultimo-1);
+                    this.ultimo = -1;
+                    labelUltimo.setText("Último Vertice Selecionado: "+ultimo);
+                }
+            }
+            
+            
+            if(ultimaAresta != -1){
+                
+                 Alert a = new Alert(Alert.AlertType.CONFIRMATION, "Deseja excluir essa aresta?",ButtonType.YES, ButtonType.NO);
+                
+                if(a.showAndWait().get() == ButtonType.YES){
+                                        
+                    pnPrincipal.getChildren().remove(LisAre.get(ultimaAresta).getAresta());
+                    LisAre.remove(ultimaAresta);
+                    this.ultimaAresta = -1;
+                    labelUltimoAresta.setText("Última Aresta Selecionado: "+ultimaAresta);
+                    flag = false;
+                }
+
+            }
+        }
+    }
+
+    @FXML
+    private void evtLimparAresta(ActionEvent event) {
+        
+        this.ultimaAresta = -1;
+        labelUltimoAresta.setText("Última Aresta Selecionado: "+ultimaAresta);
     }
     
 }
